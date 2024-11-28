3447e212713232123454652694a46bfe4b273adc
Dartie <kinskyalex@gmail.com>
2024-11-28 23:48:04 +0300

    Delete marks

--- test_product_page.py
+++ test_product_page.py
@@ -64,7 +64,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)

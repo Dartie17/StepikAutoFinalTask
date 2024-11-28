efabf014c2844d3d2bf6d83c1314b322bf584f69
Dartie <kinskyalex@gmail.com>
2024-11-28 22:33:58 +0300

    Update locators, base_page, main_page, test_product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -12,13 +13,21 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
-
-    def is_element_present(self, how, what):
+    def find_text(self, how, what):
         try:
-            self.browser.find_element(how, what)
+            text = self.browser.find_element(how, what).text
         except NoSuchElementException:
+            return "Текст не найден"
+        return text
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
 
@@ -29,19 +38,18 @@ class BasePage:
             return True
         return False
 
-    def is_disappeared(self, how, what, timeout=4):
+    def is_element_present(self, how, what):
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
             return False
         return True
 
-    def find_text(self, how, what):
-        try:
-            text = self.browser.find_element(how, what).text
-        except NoSuchElementException:
-            return "Текст не найден"
-        return text
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- pages/locators.py
+++ pages/locators.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
